9bb03b7c273303c9806df06d2f7c6e7f8a3291c2
Michael <mekhar75@gmail.com>
2014-10-09 00:56:45 +0200

    Simple relay server.

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,9 +1,36 @@
 package server;
 
+import java.util.List;
+import network.ServerNetwork;
+
 /**
+ * Simple Server. Relays received messages to all clients, including the sender.
  *
  * @author Michael
  */
 public class Server {
 
+    private ServerNetwork network;
+
+    /**
+     * Start a server.
+     */
+    public Server() {
+        network = new ServerNetwork();
+
+        while (true) {
+            try {
+                Thread.sleep(100);
+                List<byte[]> messages = network.getMessages();
+                if (!messages.isEmpty()) {
+                    for (byte[] message : messages) {
+                        network.sendMessageToAll(message);
+                    }
+                }
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+    }
 }
