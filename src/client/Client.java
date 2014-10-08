b7706b31709d6a28fe5ca6a4d4ba837b6e809361
Michael <mekhar75@gmail.com>
2014-10-09 01:13:15 +0200

    The client can now connect to a server and send and receive messages.

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,44 +1,96 @@
 package client;
 
+import gamelogic.Command;
 import gamelogic.Gamelogic;
 import gui.AsciiGraphics;
+import network.ClientNetwork;
 
 /**
  *
  * @author Michael
  */
-public class Client {
+public class Client implements Runnable {
 
-	/**
-	 * Name of client 1
-	 */
-	public final static String PLAYER1 = "Peter";
-	
-	
-	/**
-	 * Name of client 2
-	 */
-	public final static String PLAYER2 = "Klaus"; 
-	
+    /**
+     * Name of client 1
+     */
+    public final static String PLAYER1 = "Peter";
 
-	/**
-	 * size of field
-	 */
-	public final static int size = 10; 
-	
-	
-	/**
-	 * 
-	 * @param args
-	 * @throws Exception
-	 */
-	public static void main(String[] args) throws Exception {
-		
-		Gamelogic logic = new Gamelogic();
-		logic.initTestState();
-		
-		AsciiGraphics graphic = new AsciiGraphics(logic, size, size, PLAYER1, PLAYER2);
-		graphic.start();
-	}
+    /**
+     * Name of client 2
+     */
+    public final static String PLAYER2 = "Klaus";
+
+    /**
+     * size of field
+     */
+    public final static int size = 10;
+
+    private ClientNetwork network;
+    private Gamelogic gamelogic;
+    private AsciiGraphics graphic;
+    private Thread mainloop;
+
+    public Client() {
+        network = new ClientNetwork();
+        gamelogic = new Gamelogic();
+        gamelogic.initTestState();
+        mainloop = new Thread(this);
+        mainloop.setName("Clientmainloop");
+
+        graphic = new AsciiGraphics(gamelogic, size, size, PLAYER1, PLAYER2);
+        Thread graphicsThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                graphic.start();
+            }
+        });
+        graphicsThread.setName("graphicsThread");
+        graphicsThread.start();
+    }
+
+    /**
+     * Try to connect to a server. Returns true on success, false on failure.
+     *
+     * @param host
+     * @param name
+     * @return
+     */
+    public boolean connect(String host, String name) {
+        boolean success = network.tryConnect(host, 12345, name);
+        if (success) {
+            mainloop.start();
+        }
+        return success;
+    }
+
+    /**
+     * Send a command to the server.
+     *
+     * @param command
+     */
+    public void sendCommand(Command command) {
+        network.sendMessage(command.toBytes());
+    }
+
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        new Client();
+
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            Command command = Command.fromBytes(network.receiveMessage());
+            gamelogic.executeCommand(command);
+//            graphic.render();
+        }
+    }
 
 }
