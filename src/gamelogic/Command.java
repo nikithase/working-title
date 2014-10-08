7e1f3deadfd340af5b886ea04009fbd679438adc
Michael <mekhar75@gmail.com>
2014-10-09 00:33:10 +0200

    Implemented Command.toBytes() and Command.fromBytes().

--- src/gamelogic/Command.java
+++ src/gamelogic/Command.java
@@ -1,67 +1,78 @@
 package gamelogic;
 
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+
 /**
  *
  * @author Michael
  */
 public class Command {
 
-	
-	public static final String ATTACK = "attack";
-	public static final String MOVE = "move"; 
-	
-	/**
-	 * create a new command
-	 * 
-	 * @param command
-	 * @param unitId 
-	 * @param targetX 
-	 * @param targetY 
-	 */
-	public Command(String command, int unitId, int targetX, int targetY){
-		this.command = command;
-		this.unitId = unitId;
-		this.targetX = targetX;
-		this.targetY = targetY;
-	}
-	
-	/**
-	 * "move" or "attack"
-	 */
-	public String command;
+    public static final String ATTACK = "attack";
+    public static final String MOVE = "move";
 
-	/**
-	 * unique ID for a Unit
-	 */
-	public int unitId;
+    /**
+     * create a new command
+     *
+     * @param command
+     * @param unitId
+     * @param targetX
+     * @param targetY
+     */
+    public Command(String command, int unitId, int targetX, int targetY) {
+        this.command = command;
+        this.unitId = unitId;
+        this.targetX = targetX;
+        this.targetY = targetY;
+    }
 
-	/**
-	 * Target X field
-	 */
-	public int targetX;
+    /**
+     * "move" or "attack"
+     */
+    public String command;
 
-	/**
-	 * Target Y field
-	 */
-	public int targetY;
+    /**
+     * unique ID for a Unit
+     */
+    public int unitId;
 
-	/**
-	 * 
-	 * Converts the command into bytes
-	 * 
-	 * @return a array of bytes
-	 */
-	public byte[] toBytes() {
-		return null;
-	}
+    /**
+     * Target X field
+     */
+    public int targetX;
 
-	/**
-	 * 
-	 * Interprets a byte array as a command
-	 * 
-	 * @return a Command
-	 */
-	public Command fromBytes() {
-		return null;
-	}
+    /**
+     * Target Y field
+     */
+    public int targetY;
+
+    /**
+     *
+     * Converts the command into bytes
+     *
+     * @return a array of bytes
+     */
+    public byte[] toBytes() {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        buffer.write(command.equals("attack") ? 1 : 0);
+        buffer.write(unitId);
+        buffer.write(targetX);
+        buffer.write(targetY);
+        return buffer.toByteArray();
+    }
+
+    /**
+     *
+     * Interprets a byte array as a command
+     *
+     * @return a Command
+     */
+    public static Command fromBytes(byte bytes[]) {
+        String command = (bytes[0] == 1) ? "attack" : "move";
+        int unitId = bytes[1];
+        int targetX = bytes[2];
+        int targetY = bytes[3];
+        return new Command(command, unitId, targetX, targetY);
+    }
 }
