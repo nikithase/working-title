bbc4c684bec59c3a11fa21a60bb779901e6d44fc
Michael Haas <mekhar75@gmail.com>
2014-10-09 02:55:59 +0200

    Simple Sound module.

--- src/sound/Sound.java
+++ src/sound/Sound.java
@@ -1,17 +1,29 @@
 package sound;
 
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 /**
  *
  * @author Michael
  */
 public class Sound {
 
-	/**
-	 * Plays sounds.
-	 * 
-	 * @param sound Id of the sound
-	 */
-    public void playSound(String sound) {
-
+    /**
+     * Plays sounds.
+     *
+     * @param sound Id of the sound
+     */
+    public static void playSound(String sound) {
+        try {
+            Clip clip = AudioSystem.getClip();
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(sound).toURI().toURL());
+            clip.open(inputStream);
+            clip.start();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
