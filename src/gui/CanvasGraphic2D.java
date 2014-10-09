feb9a943a8445c836c55d600ae15fc8f228e2b24
Ludwig Biermann <ludi.biermann@gmail.com>
2014-10-09 14:32:31 +0200

    clean up code

--- src/gui/CanvasGraphic2D.java
+++ src/gui/CanvasGraphic2D.java
@@ -1,21 +1,34 @@
 package gui;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-import javax.swing.*;
-
+/**
+ * 
+ * @author Ludwig Biermann
+ *
+ */
 public class CanvasGraphic2D extends JPanel implements iGraphic {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9014567225901557748L;
+	
 	public final static int TILE_SIZE = 40;
 	public final static int UNITLIST_WIDTH = 200;
 	public final static int FOCUSEDUNIT_HEIGHT = 150;
-	public final static int DELTA = 0;
+    private static final String VERSION = "CanvasGraphic2D v0.1";
 	
-	
+	/**
+	 * testing funktion
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
+		
+		// compute sizes
 		int size_x = 10;
 		int size_y = 10;
 		
@@ -26,15 +39,12 @@ public class CanvasGraphic2D extends JPanel implements iGraphic {
 		int max_width =  gamefield_width + UNITLIST_WIDTH;
 		int max_height =  gamefield_height + FOCUSEDUNIT_HEIGHT;
 		
-		
+		// create Window
 		JFrame f = new JFrame("RofL");
-		
-		
 	    f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 	    
 	    
-	    
-
+	    // create Components 
 	    Gamefield field = new Gamefield(gamefield_width, gamefield_height, size_x, size_y, TILE_SIZE);
 	    UnitList list = new UnitList(UNITLIST_WIDTH, gamefield_height);
 	    FocusedUnit unit = new FocusedUnit(max_width, FOCUSEDUNIT_HEIGHT);
@@ -43,52 +53,11 @@ public class CanvasGraphic2D extends JPanel implements iGraphic {
 	    list.setBounds(gamefield_width, 0, UNITLIST_WIDTH, gamefield_height);
 	    unit.setBounds(0, gamefield_height, max_width, FOCUSEDUNIT_HEIGHT);
 	    
-	    
 	    f.add(field);
 	    f.add(list);
 	    f.add(unit);
 	    f.add(new JPanel());
 	    
-	    
-
-/*	    
-	    BoxLayout boxLayout = new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS); 
-	    
-	    f.setLayout(boxLayout);
-	    
-	    
-	    Box top = Box.createHorizontalBox();
-	    top.setBounds(0, 0, max_width, gamefield_height);
-//	    top.setAlignmentY(Component.TOP_ALIGNMENT);
-	    Gamefield field = new Gamefield(gamefield_width, gamefield_height);
-	    field.setSize(gamefield_width, gamefield_height);
-	    UnitList list = new UnitList(UNITLIST_WIDTH, gamefield_height);
-//	    list.setSize(UNITLIST_WIDTH, gamefield_height);
-	    list.setBounds(0, 0, UNITLIST_WIDTH, gamefield_height);
-	    
-	    
-//	    JButton button = new JButton("test");
-//	    JButton button2 = new JButton("test2");
-//	    button.setAlignmentY(Component.TOP_ALIGNMENT);
-//	    button2.setAlignmentY(Component.TOP_ALIGNMENT);
-//		top.add( button);
-//		top.add( button2);
-
-	   top.add( field);
-	   top.add( list);
-
-	    f.add(top);
-	    
-	    Box bot = Box.createVerticalBox();
-	    bot.setBounds(gamefield_width, gamefield_height, max_width, FOCUSEDUNIT_HEIGHT);
-
-	    bot.add( new FocusedUnit(max_width, FOCUSEDUNIT_HEIGHT) );
-
-	    f.add(bot);
-
-	    System.out.println(max_width + "x" + max_height);
-	    
-	    */
 	    f.setSize( max_width, max_height);
 	    f.setVisible( true );
 	    
