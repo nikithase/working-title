6b791bfc2976bfc27f45adbb065b220aee93e954
Michael Haas <mekhar75@gmail.com>
2014-10-11 21:15:45 +0200

    Laucher that starts a server and two clients for convinient testing.

---
+++ src/main/TwoClientsOneServer.java
@@ -0,0 +1,47 @@
+package main;
+
+import client.Client;
+import server.Server;
+
+/**
+ *
+ * @author Michael
+ */
+public class TwoClientsOneServer {
+
+    private final Thread serverThread;
+    private final Thread clientThread1;
+    private final Thread clientThread2;
+
+    public TwoClientsOneServer() {
+        serverThread = new Thread(() -> {
+            Server.main(new String[]{});
+        });
+        serverThread.setName("ServerThread");
+        serverThread.start();
+
+        try {
+            Thread.sleep(1); // use higher values for woodpc
+        } catch (InterruptedException ex) {
+
+        }
+
+        clientThread1 = new Thread(() -> {
+            Client.main(new String[]{"--left", "--Klaus"});
+        });
+        clientThread1.setName("clientThread1");
+        clientThread1.start();
+
+        clientThread2 = new Thread(() -> {
+            Client.main(new String[]{"--right", "--Peter"});
+        });
+        clientThread2.setName("clientThread2");
+        clientThread2.start();
+
+    }
+
+    public static void main(String args[]) {
+        new TwoClientsOneServer();
+    }
+
+}
